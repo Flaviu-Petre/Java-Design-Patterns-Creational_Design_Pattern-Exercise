0e41a69caf3f68db709b436760f018f8830a9449
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-09-04 14:24:20 +0300

    [added] Logger class with singleton pattern
    -Thread Safety
    -Clone Safety
    -Serialization Safety

---
+++ Java Design Patterns-Creational_Design_Pattern-Exercise/src/main/java/SingletonPattern/Logger.java
@@ -0,0 +1,40 @@
+package SingletonPattern;
+
+import java.io.Serializable;
+
+public class Logger implements Cloneable, Serializable {
+    private static Logger instance;
+
+    private Logger() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException("Cloning of this object is not allowed");
+    }
+
+    private Object readResolve() {
+        return instance;
+    }
+
+    public synchronized void log(String message) {
+        System.out.println("Log: " + message);
+    }
+
+    public synchronized void logError(String message) {
+        System.err.println("[" + Thread.currentThread().getName() + "] ERROR: " + message);
+    }
+
+    public synchronized void logInfo(String message) {
+        System.out.println("[" + Thread.currentThread().getName() + "] INFO: " +
+                java.time.LocalDateTime.now() + " - " + message);
+    }
+}
